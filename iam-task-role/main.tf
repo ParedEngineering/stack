491e44c70ec01f2ab1653bd2ea640499bc0e69ae
Chase <chase.relock@gmail.com>
2018-02-23 13:59:57 -0800

    Parameter store based

--- iam-task-role/main.tf
+++ iam-task-role/main.tf
@@ -2,60 +2,64 @@ variable "name" {
   description = "The name of the stack to use in security groups"
 }
 
+variable "cluster" {
+  description = "The name of the ecs cluster task will be placed in"
+}
+
 variable "environment" {
   description = "The name of the environment for this stack"
 }
 
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs-task-role-${var.name}-${var.environment}"
-  assume_role_policy = <<EOF
-{
-   "Version": "2012-10-17",
-   "Statement": [
-     {
-       "Sid": "",
-       "Effect": "Allow",
-       "Principal": {
-         "Service": "ecs-tasks.amazonaws.com"
-       },
-       "Action": "sts:AssumeRole"
-     }
-   ]
- }
-EOF
+variable "decrypt_parameters_policy_arn" {
+  description = "The ARN of the policy to decrypt parameter store values"
 }
 
-# We should eventually move to stricter config access policy
-resource "aws_iam_role_policy" "default_ecs_task_role_policy" {
-  name = "ecs-task-role-policy-${var.name}-${var.environment}"
-  role = "${aws_iam_role.ecs_task_role.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject",
-        "s3:AbortMultipartUpload",
-        "s3:ListMultipartUploadParts",
-        "s3:ListBucket",
-        "s3:ListBucketMultipartUploads"],
-      "Resource": "arn:aws:s3:::pared-config/*"
+resource "aws_iam_role" "task_role" {
+  name = "${var.cluster}-${var.environment}"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_assume_role_policy.json}"
+}
+
+# Let our task assume the ecs role
+data "aws_iam_policy_document" "ecs_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
-  ]
+  }
 }
-EOF
+
+data "aws_iam_policy_document" "allow_parameter_store_access" {
+  statement {
+    actions = ["ssm:GetParameters"]
+    effect = "Allow"
+    resources = ["arn:aws:ssm:*:*:parameter/${var.environment}/${var.name}/*"]
+
+  }
+}
+
+resource "aws_iam_role_policy" "parameter_store" {
+  name = "parameter-access-task-role-policy-${var.name}-${var.environment}"
+  role = "${aws_iam_role.task_role.id}"
+  policy = "${data.aws_iam_policy_document.allow_parameter_store_access.json}"
+
 }
 
 
+# Allow that role to decrypt parameters
+resource "aws_iam_role_policy_attachment" "decrypt_parameters" {
+  role       = "${aws_iam_role.task_role.name}"
+  policy_arn = "${var.decrypt_parameters_policy_arn}"
+}
 
 
 # Outputs for task role
-output "default_ecs_task_role_id" {
-  value = "${aws_iam_role.ecs_task_role.id}"
+output "default_task_role_id" {
+  value = "${aws_iam_role.task_role.id}"
 }
 
 output "arn" {
-  value = "${aws_iam_role.ecs_task_role.arn}"
+  value = "${aws_iam_role.task_role.arn}"
 }
